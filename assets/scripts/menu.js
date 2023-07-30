8a1830bb47219c3166b1943f8452c51d7af23b4e
emilianoar23 <emilianoar23@gmail.com>
2023-07-30 19:45:26 -0300

    menu

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,6 +1,11 @@
-let hamburgerMenu = document.querySelector('.hamburger-menu');
-let navMenu = document.querySelector('.nav-menu');
+let stickyMenu = document.querySelector('.js-sticky');
+let stickyOffset = window.innerHeight - stickyMenu.offsetHeight;
+let stickyClassName = 'is-sticky';
 
-hamburgerMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= stickyOffset) {
+    stickyMenu.classList.add(stickyClassName);
+  } else {
+    stickyMenu.classList.remove(stickyClassName);
+  }
 });
