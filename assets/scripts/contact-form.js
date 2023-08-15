94966f3e5e9c565f2345d2815148ae7e1fb348c3
emilianoar23 <emilianoar23@gmail.com>
2023-08-15 18:57:13 -0300

    arreglo de alerta js

--- assets/scripts/contact-form.js
+++ assets/scripts/contact-form.js
@@ -1,16 +1,22 @@
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
-  
+
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let message = document.getElementById('message').value;
-  
+
     let messageOverlay = document.getElementById('message-overlay');
     let messagePopup = document.querySelector('.message-popup');
-  
+
     messageOverlay.style.display = 'block';
-  
+
     setTimeout(function () {
-      messageOverlay.style.display = 'none';
-    }, 3000); // Ocultar después de 3 segundos
-  });
+        messageOverlay.style.display = 'none';
+    }, 3500); // Ocultar después de 3.5 segundos
+});
+
+document.querySelector('.message-overlay').addEventListener('click', function (event) {
+    if (event.target.classList.contains('message-overlay')) {
+        this.style.display = 'none';
+    }
+});
