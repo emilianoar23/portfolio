506cf6b418b0ff06dfe25f6e9c6842ae65063f61
emilianoar23 <emilianoar23@gmail.com>
2023-08-18 13:24:32 -0300

    seccion de contacto lista

--- assets/scripts/contact-form.js
+++ assets/scripts/contact-form.js
@@ -1,31 +1,30 @@
 //animacion de texto
-$(document).ready(function () {
-    let topText = $(".top-text").text().split("");
-    let bottomText = $(".bottom-text").text().split("");
-
-    $(".animated-text").css("font-family", "'Montserrat', sans-serif");
-
-    $(".top-text, .bottom-text").empty();
-
-    $.each(topText, function (i, v) {
-        $(".top-text").append($("<span>").text(v));
-    });
-
-    $.each(bottomText, function (i, v) {
-        $(".bottom-text").append($("<span>").text(v));
-    });
-
-    let numTopSpans = $(".top-text span").length;
-    let numBottomSpans = $(".bottom-text span").length;
-
-    for (let i = 0; i <= numTopSpans; i++) {
-        $(".top-text span:nth-child(" + i + ")").css("animation-delay", i / 10 + "s");
+$(document).ready(function() {
+    let $eachWord = $('.animated-text');
+    let wordCountIterator = 0;
+    let mainInterval;
+  
+    function startAnimation() {
+      resetAnimation();
+      mainInterval = setInterval(stripClass, 250);
     }
-
-    for (let i = 0; i <= numBottomSpans; i++) {
-        $(".bottom-text span:nth-child(" + i + ")").css("animation-delay", i / 10 + "s");
+  
+    function resetAnimation() {
+      wordCountIterator = 0;
+      $eachWord.addClass('concealed');
     }
-});
+  
+    function stripClass() {
+      $eachWord.eq(wordCountIterator).removeClass('concealed');
+      wordCountIterator++;
+      if (wordCountIterator >= $eachWord.length) {
+        clearInterval(mainInterval);
+      }
+    }
+  
+    $('h4').on('mouseenter', startAnimation);
+  });
+
 
 //formulario
 document.getElementById('contact-form').addEventListener('submit', function (event) {
