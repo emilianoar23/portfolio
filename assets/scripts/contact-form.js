18107b7cc47882484782bfd1f4e91c6f91bb8db9
emilianoar23 <emilianoar23@gmail.com>
2023-08-22 11:25:38 -0300

    configuracion formulario temporal

--- assets/scripts/contact-form.js
+++ assets/scripts/contact-form.js
@@ -26,7 +26,43 @@ $(document).ready(function() {
   });
 
 
-//formulario
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let message = document.getElementById('message').value;
+
+  let formData = {
+      name: name,
+      email: email,
+      message: message
+  };
+
+  fetch('https://formsubmit.co/emilianoromeroft@gmail.com', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+  })
+  .then(response => response.json())
+  .then(data => {
+      console.log('Respuesta del servidor:', data);
+
+      let messageOverlay = document.getElementById('alerta-envio');
+      messageOverlay.style.display = 'block';
+
+      setTimeout(function () {
+          messageOverlay.style.display = 'none';
+      }, 3500);
+  })
+  .catch(error => {
+      console.error('Error al enviar los datos:', error);
+  });
+});
+
 document.querySelector('.alerta-envio').addEventListener('click', function (event) {
   if (event.target.classList.contains('alerta-envio')) {
       this.style.display = 'none';
