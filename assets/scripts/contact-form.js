c698315c385678875a6c5656991d5701a9662ec2
emilianoar23 <emilianoar23@gmail.com>
2023-08-22 11:16:03 -0300

    configuracion formulario temporal

--- assets/scripts/contact-form.js
+++ assets/scripts/contact-form.js
@@ -27,25 +27,8 @@ $(document).ready(function() {
 
 
 //formulario
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let message = document.getElementById('message').value;
-
-    let messageOverlay = document.getElementById('alerta-envio');
-    let messagePopup = document.querySelector('.mensaje-popup');
-
-    messageOverlay.style.display = 'block';
-
-    setTimeout(function () {
-        messageOverlay.style.display = 'none';
-    }, 3500); // Ocultar después de 3.5 segundos
-});
-
 document.querySelector('.alerta-envio').addEventListener('click', function (event) {
-    if (event.target.classList.contains('alerta-envio')) {
-        this.style.display = 'none';
-    }
+  if (event.target.classList.contains('alerta-envio')) {
+      this.style.display = 'none';
+  }
 });
