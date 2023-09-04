034abba20927cd25e72738c820841da171db51d6
emilianoar23 <emilianoar23@gmail.com>
2023-09-04 13:12:18 -0300

    hero particles

--- assets/scripts/sections.js
+++ assets/scripts/sections.js
@@ -1,3 +1,52 @@
+//rotacion de skills
+$('.icons img').mouseenter(function () {
+  if (!$(this).hasClass('rotate-horizontal')) {
+    $(this).addClass('rotate-horizontal');
+    setTimeout(() => {
+      $(this).removeClass('rotate-horizontal');
+    }, 1000);
+  }
+});
+
+//animacion de texto contacto
+$(document).ready(function () {
+  let $eachWord = $('.animated-text');
+  let wordCountIterator = 0;
+  let mainInterval;
+
+  function startAnimation() {
+    resetAnimation();
+    mainInterval = setInterval(stripClass, 250);
+  }
+
+  function resetAnimation() {
+    wordCountIterator = 0;
+    $eachWord.addClass('concealed');
+  }
+
+  function stripClass() {
+    $eachWord.eq(wordCountIterator).removeClass('concealed');
+    wordCountIterator++;
+    if (wordCountIterator >= $eachWord.length) {
+      clearInterval(mainInterval);
+    }
+  }
+
+  $('h4').on('mouseenter', startAnimation);
+});
+
+//formulario
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('contact-form').addEventListener('submit', function () {
+    let messageOverlay = document.getElementById('alerta-envio');
+    messageOverlay.style.display = 'block';
+
+    setTimeout(function () {
+      messageOverlay.style.display = 'none';
+    }, 3500); // Ocultar después de 3.5 segundos
+  });
+});
+
 //hero section
 const particlesConfig = {
   "particles": {
@@ -124,42 +173,3 @@ const update = function () {
   requestAnimationFrame(update);
 };
 requestAnimationFrame(update);
-
-//animacion de texto contacto
-$(document).ready(function() {
-    let $eachWord = $('.animated-text');
-    let wordCountIterator = 0;
-    let mainInterval;
-  
-    function startAnimation() {
-      resetAnimation();
-      mainInterval = setInterval(stripClass, 250);
-    }
-  
-    function resetAnimation() {
-      wordCountIterator = 0;
-      $eachWord.addClass('concealed');
-    }
-  
-    function stripClass() {
-      $eachWord.eq(wordCountIterator).removeClass('concealed');
-      wordCountIterator++;
-      if (wordCountIterator >= $eachWord.length) {
-        clearInterval(mainInterval);
-      }
-    }
-  
-    $('h4').on('mouseenter', startAnimation);
-  });
-
-//formulario
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('contact-form').addEventListener('submit', function () {
-      let messageOverlay = document.getElementById('alerta-envio');
-      messageOverlay.style.display = 'block';
-
-      setTimeout(function () {
-          messageOverlay.style.display = 'none';
-      }, 3500); // Ocultar después de 3.5 segundos
-  });
-});
